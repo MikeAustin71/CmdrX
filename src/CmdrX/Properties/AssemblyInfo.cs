df24881b793c4c5a6570a15d5816f5921a4d774c
Mike Rapp <mike@paladinacs.net>
2015-10-24 13:21:33 -0500

    Changed name of test log directory to 'cmdrx'

--- src/CmdrX/Properties/AssemblyInfo.cs
+++ src/CmdrX/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CmdrX")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Console Command Exectuor")]
+[assembly: AssemblyConfiguration("Windows")]
+[assembly: AssemblyCompany("Paladin-ACS")]
 [assembly: AssemblyProduct("CmdrX")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
